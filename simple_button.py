329c75fee76d17ab071933e9d2ec132102d1b497
Dedward5 <david@zennor.co.uk>
2017-02-11 22:21:01 +0000

    Update simple_button.py
    Added no input or button pushed text messages

--- simple_button.py
+++ simple_button.py
@@ -8,6 +8,8 @@ GPIO.setup(17,GPIO.IN) #set up pin 17 for input
 
 while True:
   if GPIO.input(17): # if PIN 17 is HIGH which ==1 then
+    print ("No input")
+    else 
     print("Button Pressed")
     
 
