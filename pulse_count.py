b0b14cbbe11e223c22874dc943de733b39ef1b86
Dedward5 <david@zennor.co.uk>
2017-02-12 21:01:53 +0000

    Changes to be committed: Made a simple pulse counter

--- pulse_count.py
+++ pulse_count.py
@@ -1,17 +1,41 @@
 # Scrit to initiate GPIO and count how many times a button is pressed per min
-# See https://www.cl.cam.ac.uk/projects/raspberrypi/tutorials/robot/buttons_and_switches/
-
 # 20th Feb 2017
 
+
+##### Import libraries #####
+
 import RPi.GPIO as GPIO
+from time import sleep #enables sleep which is used later on
+
+
+##### Setup GPIO on the PI #####
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.IN) #set up pin 17 for input (button)
 
-GPIO.setup(17,GPIO.IN) #set up pin 17 for input
+##### Setup variable ######
+pulses = 0
 
-while True:
-  if GPIO.input(17): # if PIN 17 is HIGH which ==1 then
-    print ("No input")
-  else: 
-    print("Button Pressed")
-    
+##### Functions and callbacks #####
+def tacho_pulse (channel): # a callback thats called when GPIO Pin 17 rises
+	global pulses
+	print ("Pulse")
+	pulses +=1 
+
+##### Create callback for puse detection #####
+
+GPIO.add_event_detect(17, GPIO.RISING, callback=tacho_pulse) #call callback whe$
+
+
+##### Main program ######
+
+
+while pulses < 15:
+
+	print(pulses)
+
+
+GPIO.cleanup() # cleanup the GPIO on exit
+
+  
 
