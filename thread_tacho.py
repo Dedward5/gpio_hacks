a2f286923d3a1d2136eda58b8ea68578d4933181
Dedward5 <david@zennor.co.uk>
2017-05-01 12:38:06 +0000

    working single thread tacho POC

--- thread_tacho.py
+++ thread_tacho.py
@@ -1,18 +1,27 @@
+
 import time
 import threading
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.IN)
+
+
 
 global last_pulse
 last_pulse = time.time()
 global pulse_gap
-pulse_gap = 1
+pulse_gap = 99
 
 def get_pulse_gap ():
 	global pulse_gap
 	global last_pulse
 	while True:
-		GPIO.wait_for_edge(17, GPIO.RISING)
+		GPIO.wait_for_edge(18, GPIO.RISING)
+		print ("pulse")
 		time_now = time.time()
-		pusle_gap = last_pulse - time_now
+		pulse_gap = last_pulse - time_now
 		last_pulse = time_now
 	
 
@@ -24,9 +33,11 @@ gap_thread.start()
 
 
 while True:
-	#global pulse_gap
-	print (pulse_gap)
-	print("nothing")
+	global pulse_gap
+	# print (pulse_gap)
+	rpm = 0.5 /pulse_gap
+	print("RPM ",int(rpm*60))
+	# print("nothing")
 
     
 
